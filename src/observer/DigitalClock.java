20bcbd58b392d47b097b109150a47af932e16547
annarae <anna.raevskaia@metropolia.fi>
2022-03-22 23:59:39 +0200

    Observer tehtävä

---
+++ src/observer/DigitalClock.java
@@ -0,0 +1,26 @@
+package observer;
+
+public class DigitalClock implements Observer {
+
+    private ClockTimer timer;
+
+    public DigitalClock(ClockTimer ct){
+        timer = ct;
+        timer.attach(this);
+    }
+
+    @Override
+    public void update(Subject s) {
+        if (s == timer){
+            draw();
+        }
+    }
+
+    private void draw() {
+        int hour = timer.getHour();
+        int minute = timer.getMinute();
+        int second = timer.getSecond();
+
+        System.out.println(hour + ":" + minute + ":" + second);
+    }
+}
